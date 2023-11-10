fdefb5c6754d1295d704b88340ed32c760e03a5c
quigiaosu103 <52100996@student.tdtu.edu.vn>
2023-11-10 15:54:06 +0700

    db

---
+++ public/sql/genres.sql
@@ -0,0 +1,15 @@
+ 
+('vietnamese melodic rap', 'vietnamese melodic rap')
+('v-pop', 'v-pop'),
+('vietnamese trap', 'vietnamese trap'),  
+('vietnamese hip hop', 'vietnamese hip hop'),
+ 
+, 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
